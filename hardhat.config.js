364b8ea67c05f78ff90d22c5bedd330426fa18d6
v4ss <contact.v4ss@gmail.com>
2024-03-01 16:07:59 +0100

    add sepolia and hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+        accounts: [process.env.PRIV_KEY],
+      },
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIV_KEY],
+    },
+  },
 };
 
 task("accounts", "Prints the list of accounts", async () => {
